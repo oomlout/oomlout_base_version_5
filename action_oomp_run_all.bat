1020dbc864442c728479706a6198a238775162f0
Oom Lout <aaron@oomlout.com>
2026-05-17 15:13:03 +0100

    update

--- action_oomp_run_all.bat
+++ action_oomp_run_all.bat
@@ -7,8 +7,6 @@ echo Running action_make_action_only.py for configured part sources.
 echo Logging errors to %LOG_FILE%.
 echo.
 
-call :run_repo "C:\od\OneDrive\docs\warehouse_storage_tote"
-
 call :run_repo "C:\od\OneDrive\docs\parts"
 
 call :run_repo "C:\od\OneDrive\docs\computer_base"
@@ -17,9 +15,11 @@ call :run_repo "C:\od\OneDrive\docs\oomp_furniture"
 
 call :run_repo "C:\od\OneDrive\docs\oomp_tool"
 
+call :run_repo "C:\od\OneDrive\docs\oomp_packaging"
+
 call :run_repo "C:\od\OneDrive\docs\phone_base"
 
-
+call :run_repo "C:\od\OneDrive\docs\warehouse_storage_tote"
 
 call :run_repo "C:\od\OneDrive\docs\household_document_base"
 
@@ -28,6 +28,8 @@ call :run_repo "C:\gh\project_base"
 call :run_repo "C:\gh\oomp_paper_sheet"
 
 call :run_repo "C:\gh\oomp_hardware_screw_version_5"
+
+call :run_repo "C:\gh\oomp_hardware_bolt_version_5"
 
 call :run_repo "C:\gh\oomp_electrical_extension_lead_uk_socket_6_outlet_pro_elec_2068_version_5"
 
